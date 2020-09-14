798b438a4c5425a565ba0779c697de96052008aa
seoe5580 <seoe5580@wrdsb.ca>
2020-09-14 18:53:41 +0000

    Finished the temperature assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Converts temperture for users from celsius to fahrenheit
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creat a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // Asks what degree of temperture they would like to convert
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius");
+
+    // get input for user's temperature
+    int cel = input.nextInt();
+
+    // declears temperature for fahrenheit
+    int fah = (cel*9)/5+32;
+
+    // tells the user what temperture it is in fahrenheit
+    System.out.println(cel + "°C" + " is the same as " + fah + "‎°F");
+
   }
 }
